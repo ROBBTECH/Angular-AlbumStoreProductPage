754d279197a8be81f206736134452caae41069d9
RoibbinMchinzi <robbinmchinzi@gmail.com>
2019-11-03 23:54:42 +0200

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,7 +7,7 @@ import { Album } from '../album';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-albumInfo: any='Album';
+albumInfo: Album;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);

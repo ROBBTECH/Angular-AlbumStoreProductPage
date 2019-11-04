659f1a3b731cdf0d3c3dc71ef0317dcea062aa2c
RoibbinMchinzi <robbinmchinzi@gmail.com>
2019-11-04 19:16:18 +0200

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Album } from '../album';
+
 @Component({
   selector: 'app-product-tracklisting',
-  templateUrl: './product-tracklisting.component.html',
+  templateUrl: 'product-tracklisting.html',//'./product-tracklisting.component.html',
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
